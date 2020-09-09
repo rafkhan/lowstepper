a4b5a9fa590f4d4ddfcb2358b374805665b265cd
Rafael Khan <raf.khan.f@gmail.com>
2020-09-08 22:52:13 -0400

    implement morph fn hardcoded to sin/sq

--- lowstepper_firmware/src/lfo.h
+++ lowstepper_firmware/src/lfo.h
@@ -6,21 +6,48 @@
 const int INTERPOLATE_MAX = 1000;
 const int INTERPOLATE_MIN = 1;
 
-float pi = 3.14159;
-float twopi = pi * 2;
+#define PI 3.14159
+#define TWO_PI 6.28318
 
-float triangle(float phase) {
-  float unadjustedAmp = std::abs(std::fmod((phase - pi/2), twopi) - pi);
+/**
+ * All of the following functions take a phase input from 0 - TWO_PI
+ * and return a value from 0 - 1.
+ * 
+ * std::sin is also leveraged but not included because sine waves are
+ * incredibly based and just do that by default
+ * 
+ * lmao math
+ */
+double triangle(double phase) {
+  double unadjustedAmp = std::abs(std::fmod((phase - PI/2), TWO_PI) - PI);
   return unadjustedAmp * 2 -1;
 }
 
-float saw(float phase) {
-  return -1 * ((phase - pi) / pi);
+double saw(double phase) {
+  return -1 * ((phase - PI) / PI);
 }
 
-float square(float phase) {
-  if(phase >= pi) { return -1; }
+double square(double phase) {
+  if(phase >= PI) { return -1; }
   return 1;
+}
+
+/**
+ * Note to self: read your damn whiteboard, but write it here anyway
+ * 
+ * w = value between 0 and 1, weight to morph between functions f and g
+ * x = phase between 0 and 2pi
+ * f = function that is returned when w = 0
+ * g = function that is returned when w = 1
+ * 
+ * I(w, x, f, g) = f(x) + w(g(x) - f(x))
+ * I(x) = morphed wave function
+ */
+double fuckYeahLfo(double morph, double phase) {
+  double f, g;
+
+  // only sin / sq for now
+  return sin(phase) + morph * (square(phase) - sin(phase));
 }
 
 #endif
--- lowstepper_firmware/src/main.cpp
+++ lowstepper_firmware/src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 #include "lfo.h"
 
-float phase = 0.0;
+double phase = 0.0;
 elapsedMicros usec = 0;
+
+double morph = 0;
+elapsedMicros usec2 = 0;
+bool up = true;
 
 void setup() {
   analogWriteResolution(12);
@@ -10,18 +14,30 @@ void setup() {
 }
 
 void loop() {
-  float val = (saw(phase) * 2000.0) + 2050.0;
+  float val = (fuckYeahLfo(morph, phase) * 2000.0) + 2050.0;
+
   analogWrite(A22, (int)val);
   analogWrite(13, (int)val);
+
   phase = phase + 0.02;
 
-  if (phase >= twopi) {
+  if (phase >= TWO_PI) {
     phase = 0;
   }
 
   while (usec < 500) {
     // wait
     // TODO stabilize timing
+    if (morph < 1 && morph > 0) {
+      if (up) {
+        morph += 0.0001;
+      } else {
+        morph -= 0.0001;
+      }
+    } else {
+      up = !up;
+    }
   }
+
   usec = usec - 500;
 }
