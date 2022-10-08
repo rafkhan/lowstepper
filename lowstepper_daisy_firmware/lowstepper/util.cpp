6482821fcbd67dd74f45ac017bf7ee2b2287765e
Rafael Khan <raf.khan.f@gmail.com>
2022-10-07 20:14:20 -0400

    cleanup

--- lowstepper_daisy_firmware/lowstepper/util.cpp
+++ lowstepper_daisy_firmware/lowstepper/util.cpp
@@ -15,8 +15,7 @@ uint16_t mapFFII(float x, float in_min, float in_max, int out_min, int out_max) 
 // clamps value between 0 and 1
 float combinePotAndCv(float potValue, float cvValue) {
 	float adjustedCv = mapFFFF(min(cvValue, 0.87f), 0, 0.87f, -1, 1); // TODO: update 0.9 to 1 when CV inputs fixed
-	float adjustedPotValue = mapFFFF(min(potValue, 0.95f
-	), 0, 0.95f, 0, 1); // TODO: update 0.875 to 1 when CV inputs fixed
+	float adjustedPotValue = mapFFFF(min(potValue, 0.95f), 0, 0.95f, 0, 1); // TODO: update 0.875 to 1 when CV inputs fixed
 	// float adjustedPotValue = potValue;
 	float combinedValue = adjustedPotValue + adjustedCv;
   return fmin(fmax(combinedValue, 0), 1);
